42f271e535304f087f8bf8078cd622b6bc8e9506
Zareef <zareef.m@hotmail.com>
2024-10-26 10:07:02 +1100

    Add logout feature

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import { Text } from "@chakra-ui/react";
+
+export default function Page(): React.JSX.Element {
+  return <Text>Dashboard</Text>;
+}
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,13 +3,16 @@
 import Login from "@/components/login";
 import Signup from "@/components/signup";
 import { Button } from "@/components/ui/button";
-import { Toaster } from "@/components/ui/toaster";
+import { toaster, Toaster } from "@/components/ui/toaster";
+import { auth } from "@/firebase/config";
 import {
   DialogOpenChangeDetails,
   Flex,
   Heading,
   HStack,
+  Text,
 } from "@chakra-ui/react";
+import { onAuthStateChanged, User } from "firebase/auth";
 import { useState } from "react";
 
 export default function RootTemplate({
@@ -19,6 +22,10 @@ export default function RootTemplate({
 }) {
   const [signupOpen, setSignupOpen] = useState(false);
   const [loginOpen, setLoginOpen] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+
+  // ================================================================================
+  // Signup, login and logout
 
   function handleOpenSignup() {
     setSignupOpen(true);
@@ -44,30 +51,76 @@ export default function RootTemplate({
     setLoginOpen(details.open);
   }
 
+  function handleLogout() {
+    const signoutPromise = auth.signOut();
+
+    toaster.promise(signoutPromise, {
+      success: {
+        title: "Signed Out",
+        description: "Successfully signed out",
+        duration: 5000
+      },
+      error: {
+        title: "Signout Error",
+        description: "Failed to sign out",
+        duration: 5000
+      },
+      loading: {
+        title: "Signing Out...",
+        description: "Please wait"
+      }
+    });
+  }
+
+  // ================================================================================
+  // Checking for logged in user
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUser(user);
+    } else {
+      setUser(null);
+    }
+  });
+
   return (
     <>
       <Flex
         justify="space-around"
         bgColor="gray.50"
         shadow="sm"
-        minH={14}
+        minH={16}
         align="center"
         w="full"
       >
         <Heading color="teal.600">Education Portal</Heading>
-        <HStack>
-          <Button colorPalette="teal" onClick={handleOpenSignup}>
-            Sign Up
-          </Button>
-          <Button
-            color="teal.600"
-            borderColor="teal.600"
-            variant="outline"
-            onClick={handleOpenLogin}
-          >
-            Log In
-          </Button>
-        </HStack>
+        {user ? (
+          <HStack>
+            <Text>{user.email}</Text>
+            <Button
+              color="teal.600"
+              borderColor="teal.600"
+              variant="outline"
+              onClick={handleLogout}
+            >
+              Log Out
+            </Button>
+          </HStack>
+        ) : (
+          <HStack>
+            <Button colorPalette="teal" onClick={handleOpenSignup}>
+              Sign Up
+            </Button>
+            <Button
+              color="teal.600"
+              borderColor="teal.600"
+              variant="outline"
+              onClick={handleOpenLogin}
+            >
+              Log In
+            </Button>
+          </HStack>
+        )}
       </Flex>
       <Signup
         open={signupOpen}
