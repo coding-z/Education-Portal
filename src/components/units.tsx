aa6d57d3d4014ddb9482df0b2470e6ba4c72227e
Zareef <zareef.m@hotmail.com>
2024-10-26 16:14:37 +1100

    Setup supabase and fetch units

--- src/components/units.tsx
+++ src/components/units.tsx
@@ -1,36 +1,26 @@
-import { Card, Flex, Heading, VStack } from "@chakra-ui/react";
+"use client";
 
-const units = [
-  {
-    code: "UNI1234",
-    name: "Introduction to algorithms and data structures",
-  },
-  {
-    code: "UNI5678",
-    name: "Introduction to advanced database design",
-  },
-  {
-    code: "NUP0101",
-    name: "Foundations of electrical engineering",
-  },
-  {
-    code: "YEP7878",
-    name: "Research in geological science",
-  },
-  {
-    code: "MAN4040",
-    name: "Anatomy of human brains",
-  },
-];
+import { Card, Flex, Heading, VStack } from "@chakra-ui/react";
+import supabase from "../../supabase/config";
+import { useState } from "react";
 
 export default function Units() {
+  const [units, setUnits] = useState([]);
+  
+  supabase
+    .from("unit")
+    .select()
+    .then((value) => {
+      setUnits(value.data);
+    });
+
   return (
     <VStack w={["2xl", null, null, "5xl"]} align="flex-start" mt={8} gap={8}>
       <Heading size="3xl">Units</Heading>
       <Flex direction="row" justify="flex-start" wrap="wrap" gap={8}>
         {units.map((unit) => (
           <Card.Root
-            key={unit.code}
+            key={unit.id}
             variant="elevated"
             colorPalette="teal"
             w="xs"
