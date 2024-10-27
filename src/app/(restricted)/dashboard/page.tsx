a7c1469735048952394a9752c665f5c0c75b0260
Zareef <zareef.m@hotmail.com>
2024-10-27 19:21:43 +1100

    Fetch user privilege from db

--- src/app/(restricted)/dashboard/page.tsx
+++ src/app/(restricted)/dashboard/page.tsx
@@ -1,5 +1,32 @@
+"use client";
+
 import Units from "@/components/units";
+import { auth } from "@/firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import supabase from "../../../../supabase/config";
 
 export default function Page(): React.JSX.Element {
-  return (<Units />);
+  const [isTeacher, setIsTeacher] = useState<boolean>(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        supabase
+          .from("users")
+          .select("privilege")
+          .eq("email", user.email)
+          .then((value) => {
+            setIsTeacher(value.data[0].privilege === "teacher");
+          });
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>{isTeacher ? "Teacher" : "Student"}</h1>
+      <Units />
+    </>
+  );
 }
