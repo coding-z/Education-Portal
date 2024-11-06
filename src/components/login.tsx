6a84023a57e1ea7f6e535c829c0a444a42869f5b
Zareef <zareef.m@hotmail.com>
2024-11-06 19:18:17 +1100

    Implement OTP login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,41 +1,40 @@
-// import { Button } from "@/components/ui/button";
-// import {
-//   DialogBody,
-//   DialogCloseTrigger,
-//   DialogContent,
-//   DialogFooter,
-//   DialogHeader,
-//   DialogRoot,
-//   DialogTitle,
-// } from "@/components/ui/dialog";
-// import { Field } from "@/components/ui/field";
-// import { PasswordInput } from "@/components/ui/password-input";
-// import { toaster } from "@/components/ui/toaster";
-// import { DialogOpenChangeDetails, Input, PinInputValueChangeDetails, Stack, Text } from "@chakra-ui/react";
-// import { validate } from "email-validator";
-// import { ChangeEvent, FormEvent, useRef, useState } from "react";
-// import supabase from "../../supabase/config";
-// import { PinInput } from "@/components/ui/pin-input";
-
-import { DialogBackdrop, DialogBody, DialogCloseTrigger, DialogContent, DialogFooter, DialogHeader, DialogRoot, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { ChangeEvent, FormEvent, useRef, useState } from "react";
-import { DialogOpenChangeDetails, Input, PinInputValueChangeDetails } from "@chakra-ui/react";
+import {
+  DialogBackdrop,
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Field } from "@/components/ui/field";
-import { validate } from "email-validator";
-import supabase from "../../supabase/config";
 import { PinInput } from "@/components/ui/pin-input";
+import { toaster } from "@/components/ui/toaster";
+import {
+  DialogOpenChangeDetails,
+  Input,
+  PinInputValueChangeDetails,
+  VStack,
+} from "@chakra-ui/react";
+import { validate } from "email-validator";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import supabase from "../../supabase/config";
 
 export default function Login() {
   const [open, setOpen] = useState(false);
-  const ref = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
   const [otp, setOtp] = useState(["", "", "", "", "", ""]);
   const [otpSent, setOtpSent] = useState(false);
   const [otpError, setOtpError] = useState<string | null>(null);
-  
+  const submitRef = useRef<HTMLButtonElement>(null);
+  const formRef = useRef<HTMLFormElement>(null);
+
   function handleOpenChange(details: DialogOpenChangeDetails) {
     setOpen(details.open);
 
@@ -44,13 +43,13 @@ export default function Login() {
     }
   }
 
-  function getRef() {
-    return ref.current;
+  function getEmailRef() {
+    return emailRef.current;
   }
 
   function validateEmail() {
     const valid = validate(email);
-    
+
     if (valid) {
       setEmailError(null);
     } else {
@@ -79,10 +78,10 @@ export default function Login() {
 
     if (!otpSent) {
       const emailValid = validateEmail();
-  
+
       if (emailValid) {
         const { error } = await supabase.auth.signInWithOtp({ email });
-  
+
         if (error !== null) {
           setEmailError("Something went wrong. Try a different email?");
         } else {
@@ -93,203 +92,92 @@ export default function Login() {
       const { data, error } = await supabase.auth.verifyOtp({
         email,
         token: otp.join(""),
-        type: "email"
+        type: "email",
       });
 
       if (error !== null) {
-        setOtpError("Invalid passcode");
+        setOtpError("Invalid passcode. Double-check or retry after a minute.");
       } else {
         console.log(data);
         setOpen(false);
         clear();
+        toaster.success({
+          title: "Logged In Successfully",
+          duration: 5000,
+        });
       }
     }
-    
+
     setLoading(false);
   }
 
   function handleOtpChange(details: PinInputValueChangeDetails) {
     setOtp(details.value);
   }
-  
+
+  function handleAutoSubmit() {
+    // Auto submit once the entire code is typed
+    formRef.current.requestSubmit(submitRef.current);
+  }
+
   return (
-    <DialogRoot open={open} onOpenChange={handleOpenChange} placement="center" initialFocusEl={getRef}>
+    <DialogRoot
+      open={open}
+      onOpenChange={handleOpenChange}
+      placement="center"
+      initialFocusEl={getEmailRef}
+    >
       <DialogBackdrop />
       <DialogTrigger asChild colorPalette="teal">
         <Button variant="ghost">Log In</Button>
       </DialogTrigger>
       <DialogContent colorPalette="teal">
         <DialogCloseTrigger />
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} ref={formRef}>
           <DialogHeader>
             <DialogTitle>Log In</DialogTitle>
           </DialogHeader>
           <DialogBody>
-            <Field label="Email" helperText="A login code will be sent to this email" invalid={emailError !== null} errorText={emailError}>
-              <Input ref={ref} placeholder="me@example.com" value={email} onChange={handleEmailChange} />
-            </Field>
-            {
-              otpSent && 
-              <Field label="Passcode" helperText="Enter the code sent to your email" invalid={otpError !== null} errorText={otpError}>
-                <PinInput count={6} otp value={otp} onValueChange={handleOtpChange} />
+            <VStack gap={8}>
+              <Field
+                label="Email"
+                helperText="A login code will be sent to this email"
+                invalid={emailError !== null}
+                errorText={emailError}
+              >
+                <Input
+                  ref={emailRef}
+                  placeholder="me@example.com"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
               </Field>
-            }
+              {otpSent && (
+                <Field
+                  label="Passcode"
+                  helperText="Enter the code sent to your email"
+                  invalid={otpError !== null}
+                  errorText={otpError}
+                >
+                  <PinInput
+                    onValueComplete={handleAutoSubmit}
+                    autoFocus
+                    count={6}
+                    otp
+                    value={otp}
+                    onValueChange={handleOtpChange}
+                  />
+                </Field>
+              )}
+            </VStack>
           </DialogBody>
           <DialogFooter>
-            <Button loading={loading} type="submit">Log In</Button>
+            <Button ref={submitRef} loading={loading} type="submit">
+              Log In
+            </Button>
           </DialogFooter>
         </form>
       </DialogContent>
     </DialogRoot>
   );
 }
-
-// export default function Login({
-//   open,
-//   onOpenChange,
-//   onLoginSuccess,
-// }: {
-//   open: boolean;
-//   onOpenChange: (details: DialogOpenChangeDetails) => void;
-//   onLoginSuccess: () => void;
-// }) {
-//   const [email, setEmail] = useState("");
-//   const [otp, setOtp] = useState(["", "", "", "", "", ""]);
-//   const [password, setPassword] = useState("");
-//   const [errorVisible, setErrorVisible] = useState(false);
-//   const [emailInvalid, setEmailInvalid] = useState(false);
-//   const [passwordInvalid, setPasswordInvalid] = useState(false);
-//   const [emailErrorText, setEmailErrorText] = useState("");
-//   const initialFocusRef = useRef<HTMLInputElement>(null);
-
-//   // ================================================================================
-//   // Client validation
-
-//   function validateEmail(email: string) {
-//     const valid = validate(email);
-//     setEmailErrorText(email.length > 0 ? "Invalid email" : "Enter an email");
-//     setEmailInvalid(!valid);
-//     return valid;
-//   }
-
-//   function validatePassword(password: string) {
-//     const valid = password.length > 0;
-//     setPasswordInvalid(!valid);
-//     return valid;
-//   }
-
-//   // ================================================================================
-//   // Event handling
-
-//   function handleEmailChange(event: ChangeEvent<HTMLInputElement>) {
-//     setEmail(event.target.value);
-//     validateEmail(event.target.value);
-//   }
-
-//   function handleOtpChange(details: PinInputValueChangeDetails) {
-//     setOtp(details.value);
-//   }
-
-//   // ================================================================================
-//   // Form management
-
-//   function clearForm() {
-//     setEmail("");
-//     setPassword("");
-//     setErrorVisible(false);
-//     setEmailInvalid(false);
-//     setPasswordInvalid(false);
-//   }
-
-//   function handleOpenChange(details: DialogOpenChangeDetails) {
-//     clearForm();
-//     onOpenChange(details);
-//   }
-
-//   function handleSubmit(event: FormEvent<HTMLFormElement>) {
-//     event.preventDefault();
-//     setErrorVisible(false);
-
-//     if (!validateEmail(email) || !validatePassword(password)) return;
-
-//     // const loginPromise = signInWithEmailAndPassword(auth, email, password);
-//     const loginPromise = supabase.auth.signInWithPassword({ email, password });
-
-//     loginPromise
-//       .then((userCredential) => {
-//         onLoginSuccess();
-//         clearForm();
-//       })
-//       .catch((error) => {
-//         setErrorVisible(true);
-//       });
-
-//     toaster.promise(loginPromise, {
-//       success: {
-//         title: "Login Success",
-//         description: "Successfully logged in",
-//         duration: 5000,
-//       },
-//       error: {
-//         title: "Login Failure",
-//         description: "Failed to log in",
-//         duration: 5000,
-//       },
-//       loading: {
-//         title: "Signing In...",
-//         description: "Please wait",
-//       },
-//     });
-//   }
-
-//   return (
-//     <DialogRoot
-//       open={open}
-//       onOpenChange={handleOpenChange}
-//       placement="center"
-//       closeOnInteractOutside={false}
-//       initialFocusEl={() => initialFocusRef.current}
-//     >
-//       <DialogContent colorPalette="teal">
-//         <DialogCloseTrigger />
-//         <form onSubmit={handleSubmit}>
-//           <DialogHeader>
-//             <DialogTitle>Login</DialogTitle>
-//           </DialogHeader>
-//           <DialogBody>
-//             <Stack gap={6}>
-//               <Field
-//                 label="Email"
-//                 errorText={emailErrorText}
-//                 invalid={emailInvalid}
-//               >
-//                 <Input
-//                   type="email"
-//                   value={email}
-//                   onChange={handleEmailChange}
-//                   ref={initialFocusRef}
-//                   placeholder="me@example.com"
-//                 />
-//               </Field>
-//               <Field
-//                 label="Passcode"
-//                 errorText="Enter the one-time passcode you received"
-//                 invalid={passwordInvalid}
-//               >
-//                 <PinInput otp
-//                   value={otp}
-//                   onValueChange={handleOtpChange}
-//                 />
-//               </Field>
-//             </Stack>
-//           </DialogBody>
-//           <DialogFooter>
-//             <Button type="submit" colorPalette="teal">
-//               Login
-//             </Button>
-//           </DialogFooter>
-//         </form>
-//       </DialogContent>
-//     </DialogRoot>
-//   );
-// }
