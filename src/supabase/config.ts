d37e7d3b49ea20f1f25195ff38931c9150d0eab4
Zareef <zareef.m@hotmail.com>
2024-11-07 18:02:39 +1100

    Fix env var error

--- src/supabase/config.ts
+++ src/supabase/config.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import config from "../../env.json";
 
-const supabase = createClient(config.SUPABASE_URL, config.SUPABASE_KEY);
+const supabase = createClient(
+  config.SUPABASE_URL ?? process.env.SUPABASE_URL,
+  config.SUPABASE_KEY ?? process.env.SUPABASE_KEY
+);
 
 export default supabase;
