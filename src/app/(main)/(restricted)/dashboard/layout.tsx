e926f34493d5f095c4e974adbe89dfcc110a5de1
Zareef <zareef.m@hotmail.com>
2024-11-10 11:59:06 +1100

    Fix import error

--- src/app/(main)/(restricted)/dashboard/layout.tsx
+++ src/app/(main)/(restricted)/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Menu from "@/components/menu";
 import { Box, Flex, HStack, VStack } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import supabase from "../../../../supabase/config";
