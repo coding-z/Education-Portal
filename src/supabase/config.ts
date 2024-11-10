37f578dfc0d167580b6ba9991a159f081e6de171
Zareef <zareef.m@hotmail.com>
2024-11-10 11:56:16 +1100

    Fix Next.js environment variable issue

--- src/supabase/config.ts
+++ src/supabase/config.ts
@@ -1,11 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-import config from "../../env.json";
 
 const supabase = createClient(
-  // process.env.SUPABASE_URL,
-  // process.env.SUPABASE_KEY
-  config.SUPABASE_URL,
-  config.SUPABASE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY
 );
 
 export default supabase;
